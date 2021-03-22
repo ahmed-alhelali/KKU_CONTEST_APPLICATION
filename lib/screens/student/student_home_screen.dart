418b8f280b4c51fd551e95f7dc81449dc617ff2c
ahmed alhelali <ahmed43ali12@gmail.com>
2021-03-23 02:59:40 +0300

    delete unused code

--- lib/screens/student/student_home_screen.dart
+++ lib/screens/student/student_home_screen.dart
@@ -20,8 +20,6 @@ class _StudentHomeScreenState extends State<StudentHomeScreen> {
   bool menuOpen = false;
   Animation<double> _scaleAnimation;
 
-  final coursesName = [];
-
   @override
   Widget build(BuildContext context) {
     final TextDirection textDirection = Directionality.of(context);
@@ -31,7 +29,6 @@ class _StudentHomeScreenState extends State<StudentHomeScreen> {
           Tween<double>(begin: 1, end: 0.6).animate(widget.controller);
     }
     var size = MediaQuery.of(context).size;
-
     return AnimatedPositioned(
       curve: Curves.fastOutSlowIn,
       duration: widget.duration,
@@ -39,13 +36,13 @@ class _StudentHomeScreenState extends State<StudentHomeScreen> {
       bottom: 0,
       left: menuOpen
           ? (textDirection == TextDirection.ltr
-          ? 0.3 * size.width
-          : -0.3 * size.width)
+              ? 0.3 * size.width
+              : -0.3 * size.width)
           : 0,
       right: menuOpen
           ? (textDirection == TextDirection.ltr
-          ? -0.3 * size.width
-          : 0.3 * size.width)
+              ? -0.3 * size.width
+              : 0.3 * size.width)
           : 0,
       child: ScaleTransition(
         scale: _scaleAnimation,
@@ -54,109 +51,45 @@ class _StudentHomeScreenState extends State<StudentHomeScreen> {
           appBar: AppBar(
             leading: !menuOpen
                 ? IconButton(
-              icon: Icon(Icons.menu),
-              onPressed: () {
-                setState(() {
-                  widget.controller.forward();
-                  menuOpen = true;
-                });
-              },
-              color: Colors.white,
-            )
+                    icon: Icon(Icons.menu),
+                    onPressed: () {
+                      setState(() {
+                        widget.controller.forward();
+                        menuOpen = true;
+                      });
+                    },
+                    color: Colors.white,
+                  )
                 : IconButton(
-              icon: Icon(Icons.arrow_back_ios),
-              onPressed: () {
-                setState(() {
-                  widget.controller.reverse();
-                  menuOpen = false;
-                });
-              },
-              color: Colors.white,
-            ),
+                    icon: Icon(Icons.arrow_back_ios),
+                    onPressed: () {
+                      setState(() {
+                        widget.controller.reverse();
+                        menuOpen = false;
+                      });
+                    },
+                    color: Colors.white,
+                  ),
             title: Text(
               MyLocalization.of(context).getTranslatedValue("home_page_title"),
               style: textDirection == TextDirection.ltr
                   ? Utils.getUbuntuTextStyleWithSize(14)
                   : Utils.getTajwalTextStyleWithSize(14),
             ),
-            actions: [
-              IconButton(
-                icon: Icon(Icons.add),
-                onPressed: () {
-                  if (searchController.text != '') {
-                    // getOnce(searchController.text);
-                    // String name= getOnce(searchController.text);
-                    // setState(() {
-                    //   print(getOnce(searchController.text).toString());
-                    //   coursesName.add(name);
-                    //   searchController.text = '';
-                    // });
-                  } else {}
-                },
-                color: Colors.white,
-              ),
-            ],
             centerTitle: true,
             elevation: 0,
             brightness: AppTheme.darkTheme.appBarTheme.brightness,
             backgroundColor: Colors.transparent,
           ),
-          body: Column(
-            children: [
-              Container(
-                margin: EdgeInsets.symmetric(horizontal: 16),
-                child: TextFormField(
-                  controller: searchController,
-                  style: textDirection == TextDirection.ltr
-                      ? Utils.getUbuntuTextStyleWithSize(14)
-                      : Utils.getTajwalTextStyleWithSize(14),
-                  decoration: InputDecoration(
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(50),
-                      borderSide: BorderSide(color: Colors.grey),
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(50),
-                      borderSide: BorderSide(color: Colors.grey),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(50),
-                        borderSide: BorderSide(color: Colors.grey)),
-                    errorBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(50),
-                      borderSide: BorderSide(color: Colors.red),
-                    ),
-                    hintText:
-                    MyLocalization.of(context).getTranslatedValue("search"),
-                    hintStyle: textDirection == TextDirection.ltr
-                        ? Utils.getUbuntuTextStyleWithSize(12)
-                        : Utils.getTajwalTextStyleWithSize(12),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: getStudentCourses(textDirection),
-              ),
-            ],
-          ),
+          body: getStudentCourses(textDirection),
         ),
       ),
     );
   }
 
-  addCourseToStudent(String courseTitle, id) {
-    CollectionReference newCourse =
-    FirebaseFirestore.instance.collection("Courses");
-    return newCourse
-        .doc(id)
-        .set({"course_title": courseTitle})
-        .then((value) => {print("course added")})
-        .catchError((error) => print(error));
-  }
-
   Widget getStudentCourses(TextDirection textDirection) {
     CollectionReference courses =
-    FirebaseFirestore.instance.collection("Courses");
+        FirebaseFirestore.instance.collection("Courses");
     String courseID;
     return StreamBuilder<QuerySnapshot>(
       stream: courses.snapshots(),
@@ -188,7 +121,6 @@ class _StudentHomeScreenState extends State<StudentHomeScreen> {
           padding: EdgeInsets.symmetric(vertical: 6),
           children: snapshot.data.docs.map((DocumentSnapshot document) {
             final currentCourse = document.data().values;
-            // print(currentCourse);
             return InkWell(
               child: Container(
                 alignment: AlignmentDirectional.centerStart,
@@ -226,127 +158,4 @@ class _StudentHomeScreenState extends State<StudentHomeScreen> {
       },
     );
   }
-//
-// Widget getCourses(textDirection) {
-//   return ListView(
-//     children: coursesName.map((value) {
-//       return InkWell(
-//         child: Container(
-//           alignment: AlignmentDirectional.centerStart,
-//           padding: EdgeInsets.symmetric(horizontal: 30),
-//           margin: EdgeInsets.symmetric(vertical: 6, horizontal: 8),
-//           width: double.infinity,
-//           height: MediaQuery.of(context).size.height * 0.09,
-//           decoration: BoxDecoration(
-//             color: AppTheme.darkTheme.backgroundColor,
-//             borderRadius: BorderRadius.circular(15),
-//           ),
-//           child: Text(
-//             value.toString(),
-//             style: textDirection == TextDirection.ltr
-//                 ? Utils.getUbuntuTextStyleWithSize(16)
-//                 : Utils.getTajwalTextStyleWithSize(16),
-//           ),
-//         ),
-//         onTap: () {},
-//       );
-//     }).toList(),
-//   );
-// }
-//
-// Widget get(textDirection, id) {
-//   return StreamBuilder(
-//     stream:
-//         FirebaseFirestore.instance.collection("Courses").doc(id).snapshots(),
-//     builder: (context, snapshot) {
-//       if (snapshot.hasError) {
-//         return Text(
-//           MyLocalization.of(context).getTranslatedValue("error_connection"),
-//         );
-//       }
-//
-//       if (snapshot.connectionState == ConnectionState.waiting) {
-//         return Center(
-//           child: CircularProgressIndicator(),
-//         );
-//       }
-//
-//       if (snapshot.data.size == 0) {
-//         return Center(
-//           child: Text(
-//             MyLocalization.of(context).getTranslatedValue("no_courses"),
-//             style: textDirection == TextDirection.ltr
-//                 ? Utils.getUbuntuTextStyleWithSize(14)
-//                 : Utils.getTajwalTextStyleWithSize(14),
-//           ),
-//         );
-//       }
-//       var course = snapshot.data;
-//       print(course[id]);
-//       return Text(course[id]);
-//     },
-//   );
-// }
-//
-// Widget getStudentsCourse(textDirection, id) {
-//   CollectionReference courses =
-//       FirebaseFirestore.instance.collection("Courses");
-//   return StreamBuilder<QuerySnapshot>(
-//     stream: courses.snapshots(),
-//     builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-//       if (snapshot.hasError) {
-//         return Text(
-//           MyLocalization.of(context).getTranslatedValue("error_connection"),
-//         );
-//       }
-//
-//       if (snapshot.connectionState == ConnectionState.waiting) {
-//         return Center(
-//           child: CircularProgressIndicator(),
-//         );
-//       }
-//
-//       if (snapshot.data.size == 0) {
-//         return Center(
-//           child: Text(
-//             MyLocalization.of(context).getTranslatedValue("no_courses"),
-//             style: textDirection == TextDirection.ltr
-//                 ? Utils.getUbuntuTextStyleWithSize(14)
-//                 : Utils.getTajwalTextStyleWithSize(14),
-//           ),
-//         );
-//       }
-//       return ListView(
-//         padding: EdgeInsets.symmetric(vertical: 6),
-//         children: snapshot.data.docs.map(
-//           (DocumentSnapshot document) {
-//             final currentCourse = document.data().containsKey(id);
-//             // print(currentCourse);
-//             return InkWell(
-//               child: Container(
-//                 alignment: AlignmentDirectional.centerStart,
-//                 padding: EdgeInsets.symmetric(horizontal: 30),
-//                 margin: EdgeInsets.symmetric(vertical: 6, horizontal: 8),
-//                 width: double.infinity,
-//                 height: MediaQuery.of(context).size.height * 0.09,
-//                 decoration: BoxDecoration(
-//                   color: AppTheme.darkTheme.backgroundColor,
-//                   borderRadius: BorderRadius.circular(15),
-//                 ),
-//                 child: Text(
-//                   currentCourse.toString(),
-//                   style: textDirection == TextDirection.ltr
-//                       ? Utils.getUbuntuTextStyleWithSize(16)
-//                       : Utils.getTajwalTextStyleWithSize(16),
-//                 ),
-//               ),
-//               onTap: () {
-//               },
-//             );
-//           },
-//         ).toList(),
-//       );
-//     },
-//   );
-// }
 }
